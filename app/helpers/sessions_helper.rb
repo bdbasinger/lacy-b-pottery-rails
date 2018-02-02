d04a31e44a6b22912a023cdf179d6d5d735430c5
Brennan Basinger <bdbasinger777@gmail.com>
2018-02-02 08:20:51 -0600

    Finished user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,29 @@
 module SessionsHelper
+
   #Log in the given user
   def log_in(user)
     session[:user_id] = user.id
   end
 
+
+  # Remembers a user in a persistent session
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the user corresponding to the remember token cookie
   def current_user
     # If session of user id exists
     # while setting user id to session of user id
+    # Remember no comparison is taking place in the if statement below
+    # It looks like it from first glance
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
@@ -41,5 +51,22 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to a stored location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+
+
+
+
+
 
 end
