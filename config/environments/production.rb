303f936e1b1f36f8c6efdf43cb22c65968f07a0f
Brennan Basinger <bdbasinger777@gmail.com>
2018-02-26 13:35:47 -0600

    Finish sendgrid account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Configure application to use SendGrid
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://evening-forest-92061.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
